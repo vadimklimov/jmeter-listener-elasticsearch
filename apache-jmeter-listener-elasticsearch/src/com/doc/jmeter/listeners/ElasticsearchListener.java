6236dbbf7c26dbc8465e0e87880ca5566b1526b5
Vadim Klimov <Vadim.Klimov@gmail.com>
2017-06-24 14:06:20 +0100

    Migrated from Jersey client and Gson JSON processor to Elasticsearch Java client and json-smart JSON processor

---
+++ apache-jmeter-listener-elasticsearch/src/com/doc/jmeter/listeners/ElasticsearchListener.java
@@ -0,0 +1,339 @@
+package com.doc.jmeter.listeners;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.visualizers.backend.AbstractBackendListenerClient;
+import org.apache.jmeter.visualizers.backend.BackendListenerContext;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.minidev.json.JSONObject;
+
+public class ElasticsearchListener extends AbstractBackendListenerClient {
+
+	private static final String PARAMETER_NAME_ELASTICSEARCH_PROTOCOL = "elasticsearch.protocol";
+	private static final String PARAMETER_NAME_ELASTICSEARCH_HOST = "elasticsearch.host";
+	private static final String PARAMETER_NAME_ELASTICSEARCH_PORT = "elasticsearch.port";
+	private static final String PARAMETER_NAME_ELASTICSEARCH_USER = "elasticsearch.user";
+	private static final String PARAMETER_NAME_ELASTICSEARCH_PASSWORD = "elasticsearch.password";
+	private static final String PARAMETER_NAME_ELASTICSEARCH_INDEX = "elasticsearch.index";
+	private static final String PARAMETER_NAME_ELASTICSEARCH_TYPE = "elasticsearch.type";
+	private static final String PARAMETER_NAME_TIMEZONE_ID = "timezone.id";
+	private static final String PARAMETER_NAME_RESULT_EXCLUDED_ATTRIBUTES = "result.attributes.excluded";
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ElasticsearchListener.class);
+
+	private static final Object LOCK = new Object();
+
+	private static final List<String> SAMPLE_RESULT_ALL_ATTRIBUTES = Arrays.asList("Timestamp", "StartTime", "EndTime",
+			"Time", "Latency", "ConnectTime", "IdleTime", "SampleLabel", "GroupName", "ThreadName", "ResponseCode",
+			"IsResponseCodeOk", "IsSuccessful", "SampleCount", "ErrorCount", "ContentType", "MediaType", "DataType",
+			"RequestHeaders", "ResponseHeaders", "HeadersSize", "SamplerData", "ResponseMessage", "ResponseData",
+			"BodySize", "Bytes");
+
+	private String esProtocol;
+	private String esHost;
+	private int esPort;
+	private String esUser;
+	private String esPassword;
+	private String esIndex;
+	private String esType;
+	private String tzId;
+	private String srExcludedAttributes;
+
+	private RestClient esClient = null;;
+	private boolean isEsPingOk = false;
+	private List<String> resultExcludedAttributes = new ArrayList<String>();
+
+	/*
+	 * @Override public SampleResult createSampleResult(BackendListenerContext
+	 * context, SampleResult sampleResult) {
+	 * 
+	 * // Not used in current implementation return null;
+	 * 
+	 * }
+	 */
+
+	@Override
+	public Arguments getDefaultParameters() {
+		LOGGER.debug("Retrieving Elasticsearch listener parameterization");
+
+		Arguments parameters = new Arguments();
+		parameters.addArgument(PARAMETER_NAME_ELASTICSEARCH_PROTOCOL, "http");
+		parameters.addArgument(PARAMETER_NAME_ELASTICSEARCH_HOST, null);
+		parameters.addArgument(PARAMETER_NAME_ELASTICSEARCH_PORT, "9200");
+		parameters.addArgument(PARAMETER_NAME_ELASTICSEARCH_USER, null);
+		parameters.addArgument(PARAMETER_NAME_ELASTICSEARCH_PASSWORD, null);
+		parameters.addArgument(PARAMETER_NAME_ELASTICSEARCH_INDEX, null);
+		parameters.addArgument(PARAMETER_NAME_ELASTICSEARCH_TYPE, null);
+		parameters.addArgument(PARAMETER_NAME_TIMEZONE_ID, "GMT");
+		parameters.addArgument(PARAMETER_NAME_RESULT_EXCLUDED_ATTRIBUTES, null);
+
+		return parameters;
+
+	}
+
+	@Override
+	public void handleSampleResults(List<SampleResult> sampleResults, BackendListenerContext context) {
+		LOGGER.debug("Processing sample results for request to Elasticsearch server");
+
+		if (!isEsPingOk) {
+			LOGGER.debug("Sample result will not be sent to Elasticsearch server because server ping failed");
+			return;
+		}
+
+		synchronized (LOCK) {
+			for (SampleResult sampleResult : sampleResults) {
+				LOGGER.debug("Preparing sample result to send to Elasticsearch server");
+				String sampleResult4External = getSampleResult4External(sampleResult);
+
+				if (sampleResult4External == null || sampleResult4External.isEmpty()) {
+					LOGGER.debug(
+							"Sample result will not be sent to Elasticsearch server because message content is missing");
+					return;
+				}
+				HttpEntity sampleResultEntity = new StringEntity(sampleResult4External, ContentType.APPLICATION_JSON);
+
+				String sampleResultEsDocumentLocation = "/" + esIndex + "/" + esType + "/" + UUID	.randomUUID()
+																									.toString();
+
+				LOGGER.debug("Elasticsearch request - document location: " + sampleResultEsDocumentLocation);
+				LOGGER.debug("Elasticsearch request - document data:\n" + sampleResult4External);
+
+				try {
+					Response sampleResultEsDocumentResponse = esClient.performRequest("POST",
+							sampleResultEsDocumentLocation, Collections.<String, String>emptyMap(), sampleResultEntity);
+
+					LOGGER.debug("Elasticsearch response - HTTP status code: "
+							+ sampleResultEsDocumentResponse.getStatusLine()
+															.getStatusCode());
+					LOGGER.debug("Elasticsearch response - HTTP body:\n"
+							+ EntityUtils.toString(sampleResultEsDocumentResponse.getEntity()));
+				} catch (IOException e) {
+					LOGGER.error("Error when sending sample result to Elasticsearch");
+					LOGGER.error(ExceptionUtils.getStackTrace(e));
+				}
+
+			}
+		}
+
+	}
+
+	@Override
+	public void setupTest(BackendListenerContext context) throws Exception {
+		LOGGER.debug("Setting up Elasticsearch listener");
+
+		esProtocol = context.getParameter(PARAMETER_NAME_ELASTICSEARCH_PROTOCOL)
+							.trim();
+		esHost = context.getParameter(PARAMETER_NAME_ELASTICSEARCH_HOST)
+						.trim();
+		esPort = Integer.valueOf(context.getParameter(PARAMETER_NAME_ELASTICSEARCH_PORT)
+										.trim());
+		esUser = context.getParameter(PARAMETER_NAME_ELASTICSEARCH_USER)
+						.trim();
+		esPassword = context.getParameter(PARAMETER_NAME_ELASTICSEARCH_PASSWORD)
+							.trim();
+		esIndex = context	.getParameter(PARAMETER_NAME_ELASTICSEARCH_INDEX)
+							.trim()
+							.toLowerCase();
+		esType = context.getParameter(PARAMETER_NAME_ELASTICSEARCH_TYPE)
+						.trim()
+						.toLowerCase();
+		tzId = context	.getParameter(PARAMETER_NAME_TIMEZONE_ID)
+						.trim();
+		srExcludedAttributes = context	.getParameter(PARAMETER_NAME_RESULT_EXCLUDED_ATTRIBUTES)
+										.trim();
+
+		if (srExcludedAttributes != null && srExcludedAttributes.length() > 0) {
+			resultExcludedAttributes.addAll(Arrays.asList(srExcludedAttributes.split(",")));
+			resultExcludedAttributes.retainAll(SAMPLE_RESULT_ALL_ATTRIBUTES);
+			LOGGER.debug("Possible sample result attributes: " + SAMPLE_RESULT_ALL_ATTRIBUTES);
+			LOGGER.debug("Excluded sample result attributes: " + resultExcludedAttributes);
+		}
+
+		if (esUser != null && esUser.length() > 0 && esPassword != null && esPassword.length() > 0) {
+			// Basic authentication
+			LOGGER.debug("Using basic authentication when sending requests to Elasticsearch server");
+			CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+			credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(esUser, esPassword));
+
+			HttpClientConfigCallback esHttpClientConfig = new RestClientBuilder.HttpClientConfigCallback() {
+				@Override
+				public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+					return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+				}
+			};
+
+			esClient = RestClient	.builder(new HttpHost(esHost, esPort, esProtocol))
+									.setHttpClientConfigCallback(esHttpClientConfig)
+									.build();
+		} else {
+			// No authentication
+			LOGGER.debug("Using no authentication when sending requests to Elasticsearch server");
+			esClient = RestClient	.builder(new HttpHost(esHost, esPort, esProtocol))
+									.build();
+		}
+
+		try {
+			esClient.performRequest("HEAD", "/", Collections.<String, String>emptyMap());
+			isEsPingOk = true;
+			LOGGER.info("Elasticsearch ping test: Successful");
+		} catch (IOException e) {
+			isEsPingOk = false;
+			LOGGER.error("Elasticsearch ping test: Failed");
+			LOGGER.error(ExceptionUtils.getStackTrace(e));
+		}
+
+	}
+
+	@Override
+	public void teardownTest(BackendListenerContext context) throws Exception {
+		LOGGER.debug("Shutting down Elasticsearch listener");
+
+		try {
+			esClient.close();
+		} catch (IOException e) {
+			LOGGER.error("Error when closing connection to Elasticsearch");
+			LOGGER.error(ExceptionUtils.getStackTrace(e));
+		}
+
+	}
+
+	private String getSampleResult4External(SampleResult sampleResult) {
+		LOGGER.debug("Preparing sample result message");
+
+		JSONObject sampleResult4ExternalJson = new JSONObject();
+
+		for (String attributeName : SAMPLE_RESULT_ALL_ATTRIBUTES) {
+			if (!resultExcludedAttributes.contains(attributeName)) {
+				switch (attributeName) {
+				case "Timestamp":
+					sampleResult4ExternalJson.put(attributeName,
+							LocalDateTime	.ofInstant(Instant.ofEpochMilli(sampleResult.getTimeStamp()),
+									ZoneId.of(tzId))
+											.toString());
+					break;
+				case "StartTime":
+					sampleResult4ExternalJson.put(attributeName,
+							LocalDateTime	.ofInstant(Instant.ofEpochMilli(sampleResult.getStartTime()),
+									ZoneId.of(tzId))
+											.toString());
+					break;
+				case "EndTime":
+					sampleResult4ExternalJson.put(attributeName,
+							LocalDateTime	.ofInstant(Instant.ofEpochMilli(sampleResult.getEndTime()), ZoneId.of(tzId))
+											.toString());
+					break;
+				case "Time":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getTime());
+					break;
+				case "Latency":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getLatency());
+					break;
+				case "ConnectTime":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getConnectTime());
+					break;
+				case "IdleTime":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getIdleTime());
+					break;
+				case "SampleLabel":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getSampleLabel());
+					break;
+				case "GroupName":
+					sampleResult4ExternalJson.put(attributeName, sampleResult	.getSampleLabel(true)
+																				.substring(0,
+																						sampleResult.getSampleLabel(
+																								true)
+																									.indexOf(
+																											sampleResult.getSampleLabel())
+																								- 1));
+					break;
+				case "ThreadName":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getThreadName());
+					break;
+				case "ResponseCode":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getResponseCode());
+					break;
+				case "IsResponseCodeOk":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.isResponseCodeOK());
+					break;
+				case "IsSuccessful":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.isSuccessful());
+					break;
+				case "SampleCount":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getSampleCount());
+					break;
+				case "ErrorCount":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getErrorCount());
+					break;
+				case "ContentType":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getContentType());
+					break;
+				case "MediaType":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getMediaType());
+					break;
+				case "DataType":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getDataType());
+					break;
+				case "RequestHeaders":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getRequestHeaders());
+					break;
+				case "ResponseHeaders":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getResponseHeaders());
+					break;
+				case "HeadersSize":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getHeadersSize());
+					break;
+				case "SamplerData":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getSamplerData());
+					break;
+				case "ResponseMessage":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getResponseMessage());
+					break;
+				case "ResponseData":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getResponseDataAsString());
+					break;
+				case "BodySize":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getBodySizeAsLong());
+					break;
+				case "Bytes":
+					sampleResult4ExternalJson.put(attributeName, sampleResult.getBytesAsLong());
+					break;
+				}
+			}
+		}
+
+		if (sampleResult4ExternalJson != null && !sampleResult4ExternalJson.isEmpty()) {
+			return sampleResult4ExternalJson.toString();
+		} else {
+			return null;
+		}
+
+	}
+
+}
---
+++ apache-jmeter-listener-elasticsearch/src/com/doc/jmeter/listeners/SampleResult4External.java
@@ -0,0 +1,532 @@
+package com.doc.jmeter.listeners;
+
+import javax.annotation.Generated;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("org.jsonschema2pojo")
+public class SampleResult4External {
+
+	@SerializedName("Timestamp")
+	@Expose
+	private String Timestamp;
+	@SerializedName("StartTime")
+	@Expose
+	private String StartTime;
+	@SerializedName("EndTime")
+	@Expose
+	private String EndTime;
+	@SerializedName("Time")
+	@Expose
+	private Long Time;
+	@SerializedName("Latency")
+	@Expose
+	private Long Latency;
+	@SerializedName("ConnectTime")
+	@Expose
+	private Long ConnectTime;
+	@SerializedName("IdleTime")
+	@Expose
+	private Long IdleTime;
+	@SerializedName("SampleLabel")
+	@Expose
+	private String SampleLabel;
+	@SerializedName("GroupName")
+	@Expose
+	private String GroupName;
+	@SerializedName("ThreadName")
+	@Expose
+	private String ThreadName;
+	@SerializedName("ResponseCode")
+	@Expose
+	private String ResponseCode;
+	@SerializedName("IsResponseCodeOk")
+	@Expose
+	private Boolean IsResponseCodeOk;
+	@SerializedName("IsSuccessful")
+	@Expose
+	private Boolean IsSuccessful;
+	@SerializedName("SampleCount")
+	@Expose
+	private Integer SampleCount;
+	@SerializedName("ErrorCount")
+	@Expose
+	private Integer ErrorCount;
+	@SerializedName("ContentType")
+	@Expose
+	private String ContentType;
+	@SerializedName("MediaType")
+	@Expose
+	private String MediaType;
+	@SerializedName("DataType")
+	@Expose
+	private String DataType;
+	@SerializedName("RequestHeaders")
+	@Expose
+	private String RequestHeaders;
+	@SerializedName("ResponseHeaders")
+	@Expose
+	private String ResponseHeaders;
+	@SerializedName("HeadersSize")
+	@Expose
+	private Integer HeadersSize;
+	@SerializedName("SamplerData")
+	@Expose
+	private String SamplerData;
+	@SerializedName("ResponseMessage")
+	@Expose
+	private String ResponseMessage;
+	@SerializedName("ResponseData")
+	@Expose
+	private String ResponseData;
+	@SerializedName("BodySize")
+	@Expose
+	private Long BodySize;
+	@SerializedName("Bytes")
+	@Expose
+	private Long Bytes;
+
+	/**
+	 * 
+	 * @return The Timestamp
+	 */
+	public String getTimestamp() {
+		return Timestamp;
+	}
+
+	/**
+	 * 
+	 * @param Timestamp
+	 *            The Timestamp
+	 */
+	public void setTimestamp(String Timestamp) {
+		this.Timestamp = Timestamp;
+	}
+
+	/**
+	 * 
+	 * @return The StartTime
+	 */
+	public String getStartTime() {
+		return StartTime;
+	}
+
+	/**
+	 * 
+	 * @param StartTime
+	 *            The StartTime
+	 */
+	public void setStartTime(String StartTime) {
+		this.StartTime = StartTime;
+	}
+
+	/**
+	 * 
+	 * @return The EndTime
+	 */
+	public String getEndTime() {
+		return EndTime;
+	}
+
+	/**
+	 * 
+	 * @param EndTime
+	 *            The EndTime
+	 */
+	public void setEndTime(String EndTime) {
+		this.EndTime = EndTime;
+	}
+
+	/**
+	 * 
+	 * @return The Time
+	 */
+	public Long getTime() {
+		return Time;
+	}
+
+	/**
+	 * 
+	 * @param Time
+	 *            The Time
+	 */
+	public void setTime(Long Time) {
+		this.Time = Time;
+	}
+
+	/**
+	 * 
+	 * @return The Latency
+	 */
+	public Long getLatency() {
+		return Latency;
+	}
+
+	/**
+	 * 
+	 * @param Latency
+	 *            The Latency
+	 */
+	public void setLatency(Long Latency) {
+		this.Latency = Latency;
+	}
+
+	/**
+	 * 
+	 * @return The ConnectTime
+	 */
+	public Long getConnectTime() {
+		return ConnectTime;
+	}
+
+	/**
+	 * 
+	 * @param ConnectTime
+	 *            The ConnectTime
+	 */
+	public void setConnectTime(Long ConnectTime) {
+		this.ConnectTime = ConnectTime;
+	}
+
+	/**
+	 * 
+	 * @return The IdleTime
+	 */
+	public Long getIdleTime() {
+		return IdleTime;
+	}
+
+	/**
+	 * 
+	 * @param IdleTime
+	 *            The IdleTime
+	 */
+	public void setIdleTime(Long IdleTime) {
+		this.IdleTime = IdleTime;
+	}
+
+	/**
+	 * 
+	 * @return The SampleLabel
+	 */
+	public String getSampleLabel() {
+		return SampleLabel;
+	}
+
+	/**
+	 * 
+	 * @param SampleLabel
+	 *            The SampleLabel
+	 */
+	public void setSampleLabel(String SampleLabel) {
+		this.SampleLabel = SampleLabel;
+	}
+
+	/**
+	 * 
+	 * @return The GroupName
+	 */
+	public String getGroupName() {
+		return GroupName;
+	}
+
+	/**
+	 * 
+	 * @param GroupName
+	 *            The GroupName
+	 */
+	public void setGroupName(String GroupName) {
+		this.GroupName = GroupName;
+	}
+
+	/**
+	 * 
+	 * @return The ThreadName
+	 */
+	public String getThreadName() {
+		return ThreadName;
+	}
+
+	/**
+	 * 
+	 * @param ThreadName
+	 *            The ThreadName
+	 */
+	public void setThreadName(String ThreadName) {
+		this.ThreadName = ThreadName;
+	}
+
+	/**
+	 * 
+	 * @return The ResponseCode
+	 */
+	public String getResponseCode() {
+		return ResponseCode;
+	}
+
+	/**
+	 * 
+	 * @param ResponseCode
+	 *            The ResponseCode
+	 */
+	public void setResponseCode(String ResponseCode) {
+		this.ResponseCode = ResponseCode;
+	}
+
+	/**
+	 * 
+	 * @return The IsResponseCodeOk
+	 */
+	public Boolean getIsResponseCodeOk() {
+		return IsResponseCodeOk;
+	}
+
+	/**
+	 * 
+	 * @param IsResponseCodeOk
+	 *            The IsResponseCodeOk
+	 */
+	public void setIsResponseCodeOk(Boolean IsResponseCodeOk) {
+		this.IsResponseCodeOk = IsResponseCodeOk;
+	}
+
+	/**
+	 * 
+	 * @return The IsSuccessful
+	 */
+	public Boolean getIsSuccessful() {
+		return IsSuccessful;
+	}
+
+	/**
+	 * 
+	 * @param IsSuccessful
+	 *            The IsSuccessful
+	 */
+	public void setIsSuccessful(Boolean IsSuccessful) {
+		this.IsSuccessful = IsSuccessful;
+	}
+
+	/**
+	 * 
+	 * @return The SampleCount
+	 */
+	public Integer getSampleCount() {
+		return SampleCount;
+	}
+
+	/**
+	 * 
+	 * @param SampleCount
+	 *            The SampleCount
+	 */
+	public void setSampleCount(Integer SampleCount) {
+		this.SampleCount = SampleCount;
+	}
+
+	/**
+	 * 
+	 * @return The ErrorCount
+	 */
+	public Integer getErrorCount() {
+		return ErrorCount;
+	}
+
+	/**
+	 * 
+	 * @param ErrorCount
+	 *            The ErrorCount
+	 */
+	public void setErrorCount(Integer ErrorCount) {
+		this.ErrorCount = ErrorCount;
+	}
+
+	/**
+	 * 
+	 * @return The ContentType
+	 */
+	public String getContentType() {
+		return ContentType;
+	}
+
+	/**
+	 * 
+	 * @param ContentType
+	 *            The ContentType
+	 */
+	public void setContentType(String ContentType) {
+		this.ContentType = ContentType;
+	}
+
+	/**
+	 * 
+	 * @return The MediaType
+	 */
+	public String getMediaType() {
+		return MediaType;
+	}
+
+	/**
+	 * 
+	 * @param MediaType
+	 *            The MediaType
+	 */
+	public void setMediaType(String MediaType) {
+		this.MediaType = MediaType;
+	}
+
+	/**
+	 * 
+	 * @return The DataType
+	 */
+	public String getDataType() {
+		return DataType;
+	}
+
+	/**
+	 * 
+	 * @param DataType
+	 *            The DataType
+	 */
+	public void setDataType(String DataType) {
+		this.DataType = DataType;
+	}
+
+	/**
+	 * 
+	 * @return The RequestHeaders
+	 */
+	public String getRequestHeaders() {
+		return RequestHeaders;
+	}
+
+	/**
+	 * 
+	 * @param RequestHeaders
+	 *            The RequestHeaders
+	 */
+	public void setRequestHeaders(String RequestHeaders) {
+		this.RequestHeaders = RequestHeaders;
+	}
+
+	/**
+	 * 
+	 * @return The ResponseHeaders
+	 */
+	public String getResponseHeaders() {
+		return ResponseHeaders;
+	}
+
+	/**
+	 * 
+	 * @param ResponseHeaders
+	 *            The ResponseHeaders
+	 */
+	public void setResponseHeaders(String ResponseHeaders) {
+		this.ResponseHeaders = ResponseHeaders;
+	}
+
+	/**
+	 * 
+	 * @return The HeadersSize
+	 */
+	public Integer getHeadersSize() {
+		return HeadersSize;
+	}
+
+	/**
+	 * 
+	 * @param HeadersSize
+	 *            The HeadersSize
+	 */
+	public void setHeadersSize(Integer HeadersSize) {
+		this.HeadersSize = HeadersSize;
+	}
+
+	/**
+	 * 
+	 * @return The SamplerData
+	 */
+	public String getSamplerData() {
+		return SamplerData;
+	}
+
+	/**
+	 * 
+	 * @param SamplerData
+	 *            The SamplerData
+	 */
+	public void setSamplerData(String SamplerData) {
+		this.SamplerData = SamplerData;
+	}
+
+	/**
+	 * 
+	 * @return The ResponseMessage
+	 */
+	public String getResponseMessage() {
+		return ResponseMessage;
+	}
+
+	/**
+	 * 
+	 * @param ResponseMessage
+	 *            The ResponseMessage
+	 */
+	public void setResponseMessage(String ResponseMessage) {
+		this.ResponseMessage = ResponseMessage;
+	}
+
+	/**
+	 * 
+	 * @return The ResponseData
+	 */
+	public String getResponseData() {
+		return ResponseData;
+	}
+
+	/**
+	 * 
+	 * @param ResponseData
+	 *            The ResponseData
+	 */
+	public void setResponseData(String ResponseData) {
+		this.ResponseData = ResponseData;
+	}
+
+	/**
+	 * 
+	 * @return The BodySize
+	 */
+	public Long getBodySize() {
+		return BodySize;
+	}
+
+	/**
+	 * 
+	 * @param BodySize
+	 *            The BodySize
+	 */
+	public void setBodySize(Long BodySize) {
+		this.BodySize = BodySize;
+	}
+
+	/**
+	 * 
+	 * @return The Bytes
+	 */
+	public Long getBytes() {
+		return Bytes;
+	}
+
+	/**
+	 * 
+	 * @param Bytes
+	 *            The Bytes
+	 */
+	public void setBytes(Long Bytes) {
+		this.Bytes = Bytes;
+	}
+
+}
